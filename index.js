f80910f23192179c7c50a3484a92b45725edb652
arnaudambro <arnaud.ambroselli@yahoo.fr>
2019-09-25 07:07:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+import smileys from './emojis/smileys'
+import objects from './emojis/objects'
+import places from './emojis/places'
+import nature from './emojis/nature'
+import symbols from './emojis/symbols'
+
+export default {
+  ...smileys,
+  ...objects,
+  ...places,
+  ...nature,
+  ...symbols,
+}
